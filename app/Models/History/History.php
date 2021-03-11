0d5c300220304243b25a5c3be7b922f8cf74a986
Aziz Ismailov <azizismailov872872@gmail.com>
2021-03-11 17:26:28 +0600

    new: Создал таблицу и модель для Истории

---
+++ app/Models/History/History.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models\History;
+
+use App\Models\AudioBook\AudioBook;
+use App\Models\Training\Training;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    use HasFactory;
+
+    protected $table = 'histories';
+
+    protected $guarded = [];
+
+    public function historyable()
+    {
+    	return $this->morphTo();
+    }
+
+    public function audiobooks()
+    {
+    	return $this->hasMany(AudioBook::class,'id','historyable_id');
+    }
+
+    public function trainings()
+    {
+    	return $this->hasMany(Training::class,'id','historyable_id');
+    }
+
+    public function isAudiobook()
+    {
+        return ($this->historyable_type === 'App\Models\Audiobook\Audiobook') ? true : false;
+    }
+
+}
