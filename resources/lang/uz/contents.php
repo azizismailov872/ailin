a8f5bc4051cbfbef42fc2e161af53f50600c2ddd
Aziz Ismailov <azizismailov872872@gmail.com>
2021-04-04 17:06:14 +0600

    update: Заменил текст на главной странице

---
+++ resources/lang/uz/contents.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+return [
+	'aboutAilin' => ' AILIN veb-saytiga xush kelibsiz.
+ AILIN - ko\'zi ojizlar uchun WCAG 2.0 veb-saytga kirish standartlariga muvofiq yaratilgan ixtisoslashgan ta\'lim onlayn platformasi. <br>
+  Platformada o\'quv mashg\'ulotlari, audiokitoblar va podkastlar kabi manbalar hamda o\'z-o\'zini rivojlantirish uchun turli imkoniyatlar mavjud.  Bundan tashqari, platforma ko\'ngillilardan yordam olish, shuningdek ko\'ngilli mutaxassislarning psixologik terapiyasidan o\'tish imkoniyatini beradi. <br>
+  Platformaning ushbu versiyasi faqat ma\'lumot berish uchun mo\'ljallangan.  Platforma resurslarining to\'liq versiyasini yuqoridagi "KIRISH" tugmachasida maxsus ro\'yxatdan o\'tgandan keyingina olish mumkin
+
+',
+	'aboutBlind' => 'Ko\'zi ojiz va ko\'zi ojiz odamlar dunyosi ancha yopiq dunyo ekan, biz sizlarga bu dunyoga eshikni ochish va bu odamlar qanday yashashlarini va nima qilishlarini ko\'rsatish uchun bir nechta videolarni to\'pladik.
+	',
+	'targetOfCreation' => 'AILIN loyihasining maqsadi Markaziy Osiyodagi ko\'r va ko\'zi ojiz odamlar uchun ta\'lim resurslari va shaxsiy rivojlanish imkoniyatlarini birlashtirish, shuningdek, Qirg\'iziston va Markaziy Osiyoning boshqa mamlakatlarida ko\'r va ko\'zi ojiz odamlar duch keladigan hayot va qiyinchiliklarni yoritib berishdir.',
+];
--- resources/views/main/welcome.blade.php
+++ resources/views/main/welcome.blade.php
@@ -27,7 +27,7 @@
 									</div>
 									<br>
 									<div class="col-8 col-md-8 col-sm-12 col-xs-12">
-										<p tabindex="1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+										<p tabindex="1">@lang('contents.aboutAilin')</p>
 									</div>
 									<video
 										id="my-video"
@@ -57,7 +57,7 @@
 								</div>
 								<br>
 								<div class="col-8 col-md-8 col-sm-12 col-xs-12">
-									<p tabindex="1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+									<p tabindex="1">@lang('contents.aboutBlind')</p>
 								</div>
 								<nav class="nav main-nav welcome-nav flex-column">
 				  				<a class="nav-link color-black" href="#">НАЗВАНИЕ СТАТЬИ</a>
